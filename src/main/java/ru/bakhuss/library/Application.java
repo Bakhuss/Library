11c35d6e83068c57de02747d9d2d68427733566a
Bakhuss <ali.da@mail.ru>
2018-11-18 03:57:32 +0300

    servlet init;

--- src/main/java/ru/bakhuss/library/Application.java
+++ src/main/java/ru/bakhuss/library/Application.java
@@ -4,9 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ImportResource;
 import ru.bakhuss.library.controller.impl.BookControllerImpl;
 import ru.bakhuss.library.controller.impl.CatalogControllerImpl;
 import ru.bakhuss.library.controller.impl.LibraryCardControllerImpl;
@@ -14,14 +15,14 @@ import ru.bakhuss.library.controller.impl.PersonControllerImpl;
 import ru.bakhuss.library.controller.impl.SubscriberControllerImpl;
 import ru.bakhuss.library.dao.BookDao;
 import ru.bakhuss.library.dao.CatalogDao;
+import ru.bakhuss.library.dao.LibraryCardDao;
 import ru.bakhuss.library.dao.PersonDao;
 import ru.bakhuss.library.dao.SubscriberDao;
-import ru.bakhuss.library.dao.LibraryCardDao;
 import ru.bakhuss.library.error.ResponseErrorException;
 import ru.bakhuss.library.service.impl.BookServiceImpl;
 import ru.bakhuss.library.service.impl.CatalogServiceImpl;
-import ru.bakhuss.library.service.impl.PersonServiceImpl;
 import ru.bakhuss.library.service.impl.LibraryCardServiceImpl;
+import ru.bakhuss.library.service.impl.PersonServiceImpl;
 import ru.bakhuss.library.service.impl.SubscriberServiceImpl;
 
 import java.util.Date;
@@ -29,7 +30,7 @@ import java.util.Date;
 /**
  * Точка входа.
  */
-@ImportResource("spring_mvc_config.xml")
+//@ImportResource("spring_mvc_config.xml")
 @SpringBootApplication
 @ComponentScan(basePackageClasses = {
         BookServiceImpl.class, BookDao.class, BookControllerImpl.class,
@@ -39,13 +40,18 @@ import java.util.Date;
         LibraryCardServiceImpl.class, LibraryCardDao.class, LibraryCardControllerImpl.class,
         ResponseErrorException.class
 })
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
     private static ApplicationContext context;
 
     @Autowired
     public void context(ApplicationContext context) {
         Application.context = context;
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
     }
 
     public static void main(String[] args) {

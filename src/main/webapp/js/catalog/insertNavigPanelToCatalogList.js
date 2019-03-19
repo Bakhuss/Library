265237bde7c494c765d7356f9f32d859b9925542
Bakhuss <ali.da@mail.ru>
2019-03-20 01:38:14 +0300

    delete package webapp;

--- src/main/webapp/js/catalog/insertNavigPanelToCatalogList.js
+++
@@ -1,4 +0,0 @@
-var link = document.querySelector('link[rel=import]');
-var content = link.import.querySelector('#navigation');
-var node = content.cloneNode(true);
-document.body.getElementsByTagName('div').item(0).after(node);
--- src/main/webapp/js/person/insertLibraryCardToSubscriber.js
+++
@@ -1,5 +0,0 @@
-var link = document.querySelector('link[rel=import]');
-var content = link.import.querySelector('#libraryCard');
-var node = content.cloneNode(true);
-node.getElementsByTagName('h1').item(0).hidden = true;
-document.body.getElementsByTagName('div').item(0).appendChild(node);

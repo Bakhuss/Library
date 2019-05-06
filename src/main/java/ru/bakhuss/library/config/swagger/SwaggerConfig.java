bcef64781db0e2a8699e7cde4fcb080bb91ef838
Bakhuss <ali.da@mail.ru>
2019-05-07 01:29:43 +0300

    move SwaggerConfig;

--- src/main/java/ru/bakhuss/library/config/swagger/SwaggerConfig.java
+++ src/main/java/ru/bakhuss/library/config/swagger/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package ru.bakhuss.library.config;
+package ru.bakhuss.library.config.swagger;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;

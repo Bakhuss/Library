5d66f3d20ef6dc7754964fc18c0fd40e3f6bca31
Bakhuss <ali.da@mail.ru>
2019-05-06 15:12:39 +0300

    interim;

--- src/main/java/ru/bakhuss/library/web/person/controller/impl/PersonControllerImpl.java
+++ src/main/java/ru/bakhuss/library/web/person/controller/impl/PersonControllerImpl.java
@@ -3,7 +3,6 @@ package ru.bakhuss.library.web.person.controller.impl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.servlet.error.ErrorAttributes;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,16 +10,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.request.WebRequest;
-import ru.bakhuss.library.web.person.controller.PersonController;
+import ru.bakhuss.library.common.view.ResponseView;
 import ru.bakhuss.library.core.person.model.Person;
 import ru.bakhuss.library.core.person.service.PersonService;
+import ru.bakhuss.library.error.ResponseErrorException;
+import ru.bakhuss.library.web.person.controller.PersonController;
 import ru.bakhuss.library.web.person.view.PersonView;
-import ru.bakhuss.library.common.view.ResponseView;
-
-import javax.servlet.http.HttpServletRequest;
-
-import java.util.Map;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static ru.bakhuss.library.common.Util.parseLongFromString;
@@ -50,6 +45,7 @@ public class PersonControllerImpl implements PersonController {
     @Override
     @GetMapping(value = "/{id}")
     public ResponseView getPerson(@PathVariable("id") Person person) {
+        if (person == null) throw new ResponseErrorException("Person not found");
         PersonView view = personToPersonView(person);
         view.setId(person.getId().toString());
         System.out.println(person);

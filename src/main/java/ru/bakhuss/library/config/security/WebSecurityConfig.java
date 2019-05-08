b3b64bb61564e9fcdd0d324a7b695f5bc64be2b2
Bakhuss <ali.da@mail.ru>
2019-05-08 13:47:24 +0300

    interim;

--- src/main/java/ru/bakhuss/library/config/security/WebSecurityConfig.java
+++ src/main/java/ru/bakhuss/library/config/security/WebSecurityConfig.java
@@ -12,16 +12,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable()
 		    .authorizeRequests()
-			.antMatchers("/", "/api/person/*")
-				.permitAll()
+			.antMatchers("/**", "/api/person/*")
+    				.permitAll()
 			.antMatchers("/v2/api-docs", "/configuration/**")
-				.permitAll()
+	    			.permitAll()
 			.anyRequest().authenticated()
 			.and()
 		   .formLogin()
-		//	.loginPage("/login").permitAll()
 			.and()
 		  .logout()
-			.permitAll();
+		        	.permitAll();
 	}
+
+
 }
--- src/main/java/ru/bakhuss/library/error/ErrorDetails.java
+++ src/main/java/ru/bakhuss/library/error/ErrorDetails.java
@@ -1,7 +1,10 @@
 package ru.bakhuss.library.error;
 
+import lombok.Data;
+
 import java.util.Date;
 
+@Data
 public class ErrorDetails {
 	private Date timestamp;
 	private String message;
@@ -10,8 +13,4 @@ public class ErrorDetails {
 	public ErrorDetails(Date timestamp, String message, String details) {
 		super();    this.timestamp = timestamp;    this.message = message;    this.details = details;
 		}
-
-	public Date getTimestamp() {    return timestamp;   }
-	public String getMessage() {    return message;   }
-	public String getDetails() {    return details;   }
 }

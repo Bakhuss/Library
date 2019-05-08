11efb1ef79d80713ac33a666d17f347ae917e645
Bakhuss <ali.da@mail.ru>
2019-05-08 18:49:17 +0300

    interim;

--- src/main/java/ru/bakhuss/library/config/security/WebSecurityConfig.java
+++ src/main/java/ru/bakhuss/library/config/security/WebSecurityConfig.java
@@ -12,8 +12,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable()
 		    .authorizeRequests()
-//				.antMatchers("/api/**")
-//					.authenticated()
 				.antMatchers("/**")
 					.permitAll()
 				.anyRequest()

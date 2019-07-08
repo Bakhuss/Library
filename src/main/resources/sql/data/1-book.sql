f37ae02feeed80e7bfe194ba3820b9ae983d6ecf
Bakhuss <ali.da@mail.ru>
2019-07-08 04:22:17 +0300

    refactor;

--- src/main/resources/sql/data/1-book.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO book (id, version, name) VALUES (1, 0, 'book1');
-
-INSERT INTO book (id, version, name) VALUES (2, 0, 'book2');
-
-INSERT INTO book (id, version, name) VALUES (3, 0, 'book3');
--- src/main/resources/sql/data/4-book_writer.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO book_writer (book_id, person_id) VALUES ( 1, 1 );
-INSERT INTO book_writer (book_id, person_id) VALUES ( 2, 1 );
-INSERT INTO book_writer (book_id, person_id) VALUES ( 1, 2 );
-INSERT INTO book_writer (book_id, person_id) VALUES ( 1, 3 );
-INSERT INTO book_writer (book_id, person_id) VALUES ( 2, 3 );

ee96873b07b05f4397ae0f9b41c7aaf2595dcbfc
Bakhuss <ali.da@mail.ru>
2019-07-14 16:31:27 +0300

    add spring profiles dev and prod; bind spring profiles to maven profiles;

--- web/src/main/java/ru/bakhuss/library/web/ApplicationWeb.java
+++ web/src/main/java/ru/bakhuss/library/web/ApplicationWeb.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication(scanBasePackages = "ru.bakhuss.library.*")
+@Profile({"dev", "prod"})
 public class ApplicationWeb extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(ApplicationWeb.class, args);
